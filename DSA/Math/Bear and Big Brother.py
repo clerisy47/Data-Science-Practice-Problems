5cbe5e57d95f11a9e03b205994df6898801a0171
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2023-07-20 18:30:44 +0545

    update dsa

--- DSA/Math/Bear and Big Brother.py
+++ DSA/Math/Bear and Big Brother.py
@@ -1,9 +1,3 @@
-# Bear Limak wants to become the largest of bears, or at least to become larger than his brother Bob.
-
-# Right now, Limak and Bob weigh a and b respectively. It's guaranteed that Limak's weight is smaller than or equal to his brother's weight.
-
-# Limak eats a lot and his weight is tripled after every year, while Bob's weight is doubled after every year.
-
 a, b = map(int, input().split())
 
 years = 0
